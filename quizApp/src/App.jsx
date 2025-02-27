cf0b415073c3c88c46cfd8791efa01f318834e92
ashna23 <ashnaraikwar9@gmail.com>
2025-02-28 00:45:10 +0530

    quizApp

---
+++ quizApp/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Quiz from "./components/Quiz/quiz";
+const App = ()=>{
+  return(
+    <>
+<Quiz/>
+    </>
+  )
+}
+export default App
---
+++ quizApp/src/assets/Data.js
@@ -0,0 +1,55 @@
+export const data = [
+
+
+    {
+        question: "What is the correct file extension for Python files?",
+        option1: ".pyth",
+        option2: ".pt",
+        option3: ".py",
+        option4: ".pyt",
+
+        ans: 3
+    },
+    {
+        question: "Which of the following is a correct variable declaration in Python?",
+
+        option1: "int a = 5",
+        option2: "a = 5",
+        option3: "declare a = 5",
+        option4: "var a = 5",
+
+        ans: 2
+    },
+    {
+        question: "What is the output of print(2 ** 3)?",
+
+        option1: "5",
+        option2: "6",
+        option3: "8",
+        option4: "9",
+
+        ans:3
+    },
+    {
+        question: "Which keyword is used to define a function in Python?",
+
+        option1: "func",
+        option2: "define",
+        option3: "function",
+        option4: "def",
+
+        ans: 4
+    },
+    {
+        question: "What data type is the object below? L = [1, 23, 'hello', 1]",
+    
+            option1: "List",
+            option2: "Dictionary",
+            option3: "Tuple",
+            option4: "Array",
+        
+        ans: 1
+    }
+
+
+]
